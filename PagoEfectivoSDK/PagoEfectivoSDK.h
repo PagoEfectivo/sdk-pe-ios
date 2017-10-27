4d1ca0968eb1eb96a0490a07b6595b48a531b35c
andru255 <andru255@gmail.com>
2017-10-27 10:21:17 -0500

    first commit

---
+++ PagoEfectivoSDK/PagoEfectivoSDK.h
@@ -0,0 +1,19 @@
+//
+//  PagoEfectivoSDK.h
+//  PagoEfectivoSDK
+//
+//  Created by Andres on 10/26/17.
+//  Copyright © 2017 orbis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PagoEfectivoSDK.
+FOUNDATION_EXPORT double PagoEfectivoSDKVersionNumber;
+
+//! Project version string for PagoEfectivoSDK.
+FOUNDATION_EXPORT const unsigned char PagoEfectivoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PagoEfectivoSDK/PublicHeader.h>
+
+
